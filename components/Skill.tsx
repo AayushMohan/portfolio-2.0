a59e5a1a31e1ab75fd67b04033ecbd9afa82bc59
AayushMohan <aayushmohan1702@gmail.com>
2022-09-19 23:46:23 +0530

    Added: Hover Effect In Skill

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -18,6 +18,11 @@ const Skill = ({ directionLeft }: Props) => {
         src="https://avatars.githubusercontent.com/u/17177659?s=280&v=4"
         className="rounded-full border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
       />
+      <div className="absolute opacity-0 group-hover:opacity-80 duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:h-28 md:w-28 xl:w-32 xl:h-32 rounded-full z-0">
+        <div className="flex items-center justify-center h-full">
+          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+        </div>
+      </div>
     </div>
   );
 };
