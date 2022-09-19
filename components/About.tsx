310c381ae7317a81b35fd1d3dc2409df7fc59434
AayushMohan <aayushmohan1702@gmail.com>
2022-09-19 22:17:42 +0530

    Added: About Section Details

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,50 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 function About({}: Props) {
-  return <div>About</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+
+      <motion.img
+        initial={{ x: -200, opacity: 0 }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
+        src="https://avatars.githubusercontent.com/u/66319691?v=4"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95  xl:w-[500px] xl:h-[600px]"
+      />
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="font-semibold text-4xl">
+          Here is a{" "}
+          <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
+          background
+        </h4>
+        <p className="text-base">
+          {`
+           I'm a Frontend Web Developer with a passion for outstanding design and
+           technology. I like to think of myself as a leader who is willing to go
+           above and beyond to see that our product goal is realized. I'm
+           extremely enthusiastic about design and carrying it through to
+           completion. I'm a big fan of science and technology, so I strive to
+           stay on top of things so that our consumers get the greatest
+           experience possible. Currently expanding my horizon in 
+           UI & UX.
+          `}
+        </p>
+      </div>
+    </motion.div>
+  );
 }
 
 export default About;
