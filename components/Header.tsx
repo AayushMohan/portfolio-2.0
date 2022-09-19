69993ef94e044b26af9773f5f2070e0a831ab86a
AayushMohan <aayushmohan1702@gmail.com>
2022-09-19 12:35:17 +0530

    Added: Social Icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
 function Header({}: Props) {
-  return <div>I am a Header</div>;
+  return (
+    <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center p-5">
+      <div className="flex flex-row items-center">
+        {/* Social Icons */}
+        <SocialIcon
+          url="https://twitter.com/AayushMohan"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://twitter.com/AayushMohan"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://twitter.com/AayushMohan"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://twitter.com/AayushMohan"
+          fgColor="gray"
+          bgColor="transparent"
+        />{" "}
+      </div>
+      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch
+        </p>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
