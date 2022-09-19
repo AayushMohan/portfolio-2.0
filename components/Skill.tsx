e9c57cb428bb3695477e978523a49d3c47c101c8
AayushMohan <aayushmohan1702@gmail.com>
2022-09-19 23:32:45 +0530

    Added: Skill Details

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { motion } from "framer-motion";
 
-type Props = {};
+type Props = {
+  directionLeft: boolean;
+};
 
-const Skill = (props: Props) => {
-  return <div>Skill</div>;
+const Skill = ({ directionLeft }: Props) => {
+  return (
+    <div className="group relative flex cursor-pointer">
+      <motion.img
+        initial={{
+          x: directionLeft ? -200 : 200,
+          opacity: 0,
+        }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        src="https://avatars.githubusercontent.com/u/17177659?s=280&v=4"
+      />
+    </div>
+  );
 };
 
 export default Skill;
