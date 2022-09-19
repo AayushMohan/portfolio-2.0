08263d1608d492c61d1ada7b8e7ccea8e1482e83
AayushMohan <aayushmohan1702@gmail.com>
2022-09-19 15:14:04 +0530

    Completed Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
 
 type Props = {};
 
@@ -15,11 +18,39 @@ const Hero = (props: Props) => {
   });
 
   return (
-    <div>
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <Image
+        src="https://avatars.githubusercontent.com/u/66319691?v=4"
+        alt=""
+        height={128}
+        width={128}
+        className="relative rounded-full mx-auto object-cover"
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Developer
+        </h2>
+
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
