412b22252345572c39e9efebddea90caa6cc6a16
AayushMohan <aayushmohan1702@gmail.com>
2022-09-20 15:42:32 +0530

    Added: Projects Details

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,13 +1,35 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
 
 function Projects({}: Props) {
+  const projects = [1, 2, 3, 4, 5];
   return (
     <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
+
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project) => (
+          <div
+            key={project}
+            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
+          >
+            <Image
+              src="https://aayushmohan.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FOS.a24d71f1.png&w=3840&q=75"
+              alt=""
+              width="550"
+              height="450"
+            />
+
+            <div>
+              <h4>Case Study 1 Of 3: OpenSea Clone</h4>
+            </div>
+          </div>
+        ))}
+      </div>
 
       <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12" />
     </div>
