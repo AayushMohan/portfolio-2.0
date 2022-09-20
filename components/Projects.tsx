aa8ff76d7533d9b368e2d984f17c59933c1c0b4c
AayushMohan <aayushmohan1702@gmail.com>
2022-09-20 18:38:08 +0530

    Added: Project Description

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -12,7 +12,7 @@ function Projects({}: Props) {
       </h3>
 
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
-        {projects.map((project) => (
+        {projects.map((project, i) => (
           <div
             key={project}
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
@@ -24,8 +24,19 @@ function Projects({}: Props) {
               height="450"
             />
 
-            <div>
-              <h4>Case Study 1 Of 3: OpenSea Clone</h4>
+            <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+              <h4 className="text-4xl font-semibold text-center">
+                <span className="underline decoration-[#F7AB0A]/50">
+                  Case Study {i + 1} Of {projects.length} :
+                </span>{" "}
+                OpenSea Clone
+              </h4>
+
+              <p className="">
+                This site is a clone of Opensea, the first and largest NFT
+                marketplace in the world. Users can use Metamask to authenticate
+                and purchase samples from the collection.
+              </p>
             </div>
           </div>
         ))}
