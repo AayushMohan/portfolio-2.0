3eb7d81eabd52f9733c4d48c3eee441f58cf1480
AayushMohan <aayushmohan1702@gmail.com>
2022-09-22 19:43:00 +0530

    Added: Contact me text

--- components/About.tsx
+++ components/About.tsx
@@ -21,7 +21,7 @@ function About({}: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src="https://avatars.githubusercontent.com/u/66319691?v=4"
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95  xl:w-[500px] xl:h-[600px]"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95  xl:w-[400px] xl:h-[500px]"
       />
 
       <div className="space-y-10 px-0 md:px-10">
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -10,9 +10,9 @@ const Skills = (props: Props) => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
+      className="h-[1000px] flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 justify-center xl:space-y-0 mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
         Skills
       </h3>
 
@@ -20,7 +20,7 @@ const Skills = (props: Props) => {
         Hover over a skill for current proficiency
       </h3>
 
-      <div className="grid grid-cols-4 gap-5">
+      <div className="absolute top-1/4 grid grid-cols-4 gap-5">
         <Skill />
         <Skill />
         <Skill />
