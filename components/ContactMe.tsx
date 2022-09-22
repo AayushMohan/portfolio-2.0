2e0e70b4478ccc224640cf0e7885e3eb9f79c286
AayushMohan <aayushmohan1702@gmail.com>
2022-09-22 22:19:42 +0530

    Added: Email Details

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -16,10 +16,20 @@ const ContactMe = (props: Props) => {
           <span className="decoration-[#F7AB0A]/50 underline">Lets Talk.</span>
         </h4>
 
-        <div>
+        <div className="space-y-10">
           <div className="flex items-center space-x-5 justify-center">
-            <PhoneIcon className="h-7 w-7" />
-            <p>+1234567890</p>
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7" />
+            <p className="text-2xl">+1234567890</p>
+          </div>
+
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">aayushmohan1702@gmail.com</p>
+          </div>
+
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">123 Developer Lane</p>
           </div>
         </div>
       </div>
