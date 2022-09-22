213709bede6482941bc2bd7e49d0ba1bfecbb10e
AayushMohan <aayushmohan1702@gmail.com>
2022-09-22 23:30:16 +0530

    Added: Mailto Feature

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -14,7 +14,9 @@ type Props = {};
 const ContactMe = ({}: Props) => {
   const { register, handleSubmit } = useForm<Inputs>();
 
-  const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:aayushmohan1702@gmail?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`;
+  };
 
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
