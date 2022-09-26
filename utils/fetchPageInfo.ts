52c79c6a315244e1d65f079ac82691e33847c738
AayushMohan <aayushmohan1702@gmail.com>
2022-09-26 13:16:29 +0530

    Added: Fetch Experience

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -6,6 +6,6 @@ export const fetchPageInfo = async () => {
  const data = await res.json();
  const pageInfo: PageInfo = data.pageInfo;
 
- // console.log("fetching", projects); 
+ // console.log("fetching", pageInfo); 
  return pageInfo;
 }
