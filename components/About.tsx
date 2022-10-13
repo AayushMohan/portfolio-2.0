476f874ca5e532997c51a3b73a096f496a73d053
AayushMohan <aayushmohan1702@gmail.com>
2022-10-14 01:15:53 +0530

    Fixed: About Styling Bug

--- components/About.tsx
+++ components/About.tsx
@@ -15,7 +15,7 @@ function About({ pageInfo }: Props) {
       transition={{ duration: 1.5 }}
       className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 py-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="text-left md:absolute top-24 md:-ml-20 uppercase tracking-[20px] text-gray-500 text-2xl mt-10">
         About
       </h3>
 
@@ -25,16 +25,18 @@ function About({ pageInfo }: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src={urlFor(pageInfo?.profilePic).url()}
-        className="-mb-20 md:mb-0 flex-shrink-0 w-40 h-40 rounded-full object-cover md:rounded-lg md:w-64 md:h-95  xl:w-[400px] xl:h-[500px]"
+        className="-mb-10 md:mb-0 flex-shrink-0 w-40 h-40 rounded-full object-cover md:rounded-lg md:w-64 md:h-95  xl:w-[400px] xl:h-[500px]"
       />
 
-      <div className="space-y-10 px-0 md:px-10">
-        <h4 className="font-semibold text-4xl">
+      <div className="space-y-10 px-0 md:px-16 md:text-left text-center">
+        <h4 className="font-semibold text-4xl m my-10">
           Here is a{" "}
           <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
           background
         </h4>
-        <p className="text-base">{pageInfo?.backgroundInformation}</p>
+        <p className="text-base leading-relaxed md:leading-loose">
+          {pageInfo?.backgroundInformation}
+        </p>
       </div>
     </motion.div>
   );
