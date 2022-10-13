bc39c7c3857a1c2f814827c678f01f58bb5ed54e
AayushMohan <aayushmohan1702@gmail.com>
2022-10-14 02:26:35 +0530

    Fixed: Skills Styling

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -13,17 +13,17 @@ const Skills = ({ skills }: Props) => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="flex relative flex-col text-center md:text-left xl:flex-row w-screen max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-2 items-center"
+      className="flex relative flex-col text-center md:text-left xl:flex-row w-screen max-w-[2000px] xl:px-10 h-screen justify-center xl:space-y-10 mx-2 items-center pb-10"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
         Skills
       </h3>
 
-      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm mx-8">
         Hover over a skill for current proficiency
       </h3>
 
-      <div className="absolute top-1/4 grid grid-cols-4 gap-4 h-1/2 py-10">
+      <div className="absolute top-1/4 grid grid-cols-4 md:grid-cols-5 gap-4 md:gap-8 h-1/2 py-5">
         {skills?.slice(0, skills.length / 2).map((skill) => (
           <Skill key={skill._id} skill={skill} directionLeft={false} />
         ))}
