ee962a7cde39b29da85742070d8638bb92ec1f82
AayushMohan <aayushmohan1702@gmail.com>
2022-10-19 23:12:47 +0530

    Added: Hover Styling

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -23,9 +23,9 @@ const Skill = ({ skill, directionLeft }: Props) => {
         className="rounded-full border border-gray-500 object-cover w-16 h-16 md:w-24 md:h-24 xl:w-24 xl:h-24 filter group-hover:grayscale transition duration-300 ease-in-out p-2"
         alt=""
       />
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:h-24 md:w-24 xl:w-24 xl:h-24 rounded-full z-0">
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-16 w-16  md:h-24 md:w-24 xl:w-24 xl:h-24 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
-          <p className="text-3xl font-bold text-black opacity-100">
+          <p className="text-2xl text-center font-bold text-black opacity-100">
             {skill.progress}%
           </p>
         </div>
