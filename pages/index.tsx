c3496b3734fb5a0f46579fef39a1c97cb8f022e8
AayushMohan <aayushmohan1702@gmail.com>
2022-10-19 23:53:56 +0530

    Updated: Index

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
-import { Experience, PageInfo, Project, Skill, Social } from "../typings";
+import { PageInfo, Project, Skill, Social } from "../typings";
 import { fetchExperiences } from "../utils/fetchExperiences";
 import { fetchPageInfo } from "../utils/fetchPageInfo";
 import { fetchProjects } from "../utils/fetchProjects";
